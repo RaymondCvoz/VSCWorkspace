43b32f2253b33f015e586b08f8ff1a954af85df5
RaymondCvoz <sranchvook@outlook.com>
2021-05-14 20:09:12 +0800

    refresh

--- mess/test.cpp
+++ mess/test.cpp
@@ -1,29 +1,6 @@
-#include <iostream>
-#include <algorithm>
+#include <cstdio>
 
-const int lens = 1e6 + 1;
-int ances[lens];
-
-int find(int x)
-{
-    if (ances[x] != x)
-    {
-        ances[x] = find(ances[x]);
-    }
-    return ances[x];
-}
-
-void merge(int a, int b)
-{
-    a = find(a);
-    b = find(b);
-    ances[a] = b;
-}
 int main()
 {
-    int x;
-    while (ances[x] != x)
-    {
-        ances[x] = ances[ances[x]];
-    }
+    puts("233");
 }
